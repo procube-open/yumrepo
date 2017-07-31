3e59db94131d09f4604d4de5ec3754e887d17e54
Mitsuru Nakakawaji <mitsuru@procube.jp>
2017-07-31 13:31:15 +0900

    fix lost SIGTERM problem

--- start.sh
+++ start.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+echo "Setting up yum repository.";
 make
-httpd -D FOREGROUND
+
+_term() {
+  echo "Caught SIGTERM signal!"
+  kill -TERM "$child" 2>/dev/null
+}
+
+trap _term SIGTERM
+
+echo "Start Web server.";
+httpd -D FOREGROUND &
+
+child=$!
+wait "$child"
